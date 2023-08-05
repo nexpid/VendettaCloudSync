55bfad33b8c72a3c1865b66e22e9c036bf00fc7a
Gabe616 <poggerschampersqwq@gmail.com>
2023-08-05 12:26:33 +0200

    feat: change redirectURL

--- src/util/constants.ts
+++ src/util/constants.ts
@@ -1,6 +1,6 @@
 export default {
 	oauth2: {
 		clientId: '1120793656878714913',
-		redirectURL: 'https://vd-cloudsync.nexpid.workers.dev/api/oauth2-response',
+		redirectURL: 'https://cloudsync.nexpid.xyz/api/oauth2-response',
 	},
 };
